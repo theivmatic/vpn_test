0ab9be2417e579c23de1ecb576709f16e108104f
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-09-06 12:09:59 +0500

    created assets path class

---
+++ lib/core/constants/assets_path.dart
@@ -0,0 +1,9 @@
+abstract class AssetsPath {
+  static const String hideBrowsingHistoryIcon =
+      'assets/icons/hide_browser_history.webp';
+  static const String secureDigitalConversationsIcon =
+      'assets/icons/secure_digital_conversations.webp';
+  static const String encryptOnlineData =
+      'assets/icons/encrypt_online_data.webp';
+  static const String markIpAddress = 'assets/icons/mark_ip_address.webp';
+}
