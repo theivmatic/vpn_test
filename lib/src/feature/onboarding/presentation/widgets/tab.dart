1d127cd1e2359679b5d5977e04b9e46d53147dcc
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-09-06 10:58:09 +0500

    created tab widget

---
+++ lib/src/feature/onboarding/presentation/widgets/tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TabWidget extends StatelessWidget {
+  const TabWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(),
+    );
+  }
+}
